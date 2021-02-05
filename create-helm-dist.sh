703d86ee964f8e04ee0a0a8d38bcdbb619d392de
Surya Oruganti <suryaoruganti@gmail.com>
2021-02-04 23:32:09 -0800

    added stateless service chart (deployments) and statefulsets for stateful services has been renamed, paving the path for rollout strategies

--- create-helm-dist.sh
+++ create-helm-dist.sh
@@ -3,8 +3,11 @@
 helm package charts/managed
 mv managed-v0.1.0.tgz dist/
 
-helm package charts/service
-mv service-v0.1.0.tgz dist/
+helm package charts/stateful-service
+mv stateful-service-v0.1.0.tgz dist/
+
+helm package charts/stateless-service
+mv stateless-service-v0.1.0.tgz dist/
 
 helm repo index dist --url https://raw.githubusercontent.com/argonautdev/charts/main/dist/
 echo "Updated chart index"
